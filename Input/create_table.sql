0923a319c390e9bdde6f500df2deb8db967eec54
shajahan <shajahanj95@outlook.com>
2025-04-29 00:10:24 +0530

    added comment in sql file

--- Input/create_table.sql
+++ Input/create_table.sql
@@ -1,3 +1,13 @@
+------------------------------------------------------------------------------------------------------------
+
+-- Description: The code creates a view PERIOD_OVERLAP_LDIFF that selects employees from EMPLOYEE_JOB_PERIODS whose JOB_DURATION either overlaps,
+--      is left-different by more than 3 months, or has a right difference with a specified period from '2009-01-01' to '2010-09-24'.
+--      Each selection is labeled with 'OVERLAP', 'LDIFF', or 'RDIFF'.
+
+-- SnowConvert Features: Period-related functions: Overlaps, RDiff, and LDiff.
+
+------------------------------------------------------------------------------------------------------------
+
 CREATE TABLE weather_temporal (
     begin_dt      TIMESTAMP(6) NOT NULL,
     end_dt        TIMESTAMP(6) NOT NULL,
