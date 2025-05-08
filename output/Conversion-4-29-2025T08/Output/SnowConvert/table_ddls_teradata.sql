b41b32fe5ee5d9d38f316ad0f5675bde89bf24d4
shajahan <shajahanj95@outlook.com>
2025-05-08 16:04:42 +0530

    sql files added

---
+++ output/Conversion-4-29-2025T08/Output/SnowConvert/table_ddls_teradata.sql
@@ -0,0 +1,74 @@
+﻿--** SSC-FDM-TD0024 - SET TABLE FUNCTIONALITY NOT SUPPORTED. TABLE MIGHT HAVE DUPLICATE ROWS **
+CREATE OR REPLACE TABLE DEV.SNOWCONVERT.trips
+(
+    bikeid INTEGER,
+    checkout_time VARCHAR(50),
+    duration_minutes INTEGER,
+    end_station_id INTEGER,
+    end_station_name VARCHAR(100),
+    MONTH_NUM INTEGER,
+    start_station_id INTEGER,
+    start_station_name VARCHAR(100),
+    start_time TIMESTAMP(6),
+    subscriber_type VARCHAR(50),
+    trip_id BIGINT,
+    YEAR_NUM INTEGER)
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 5,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "05-08-2025",  "domain": "elait" }}'
+;
+
+--** SSC-FDM-TD0024 - SET TABLE FUNCTIONALITY NOT SUPPORTED. TABLE MIGHT HAVE DUPLICATE ROWS **
+CREATE OR REPLACE TABLE DEV.SNOWCONVERT.Weather
+(
+    dt TIMESTAMP(6),
+    temp FLOAT,
+    temp_min FLOAT,
+    temp_max FLOAT,
+    pressure INTEGER,
+    humidity INTEGER,
+    wind_speed INTEGER,
+    wind_deg INTEGER,
+    rain_1h FLOAT,
+    rain_3h FLOAT,
+    clouds INTEGER,
+    weather_id INTEGER,
+    weather_main VARCHAR(50),
+    weather_desc VARCHAR(50),
+    weather_icon VARCHAR(50)
+)
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 5,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "05-08-2025",  "domain": "elait" }}'
+;
+
+--** SSC-FDM-TD0024 - SET TABLE FUNCTIONALITY NOT SUPPORTED. TABLE MIGHT HAVE DUPLICATE ROWS **
+CREATE OR REPLACE TABLE DEV.SNOWCONVERT.stations
+(
+    latitude DECIMAL(18,5),
+    longitude DECIMAL(18,5),
+    location GEOGRAPHY,
+    name VARCHAR(100),
+    station_id INTEGER,
+    status VARCHAR(50)
+)
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 5,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "05-08-2025",  "domain": "elait" }}'
+;
+
+CREATE OR REPLACE TABLE DEV.SNOWCONVERT.weather_temporal (
+    begin_dt TIMESTAMP(6) NOT NULL,
+    end_dt TIMESTAMP(6) NOT NULL,
+    temp FLOAT,
+    temp_min FLOAT,
+    temp_max FLOAT,
+    pressure INTEGER,
+    humidity INTEGER,
+    wind_speed INTEGER,
+    wind_deg INTEGER,
+    rain_1h FLOAT,
+    rain_3h FLOAT,
+    clouds INTEGER,
+    weather_id INTEGER,
+    weather_main VARCHAR(50),
+    weather_desc VARCHAR(50),
+    weather_icon VARCHAR(50)
+   )
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 5,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "05-08-2025",  "domain": "elait" }}'
+;
+
---
+++ output/Conversion-4-29-2025T08/Output/SnowConvert/view_ddls_teradata.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE VIEW DEV.SNOWCONVERT.austin_weather
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 5,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "05-08-2025",  "domain": "elait" }}'
+AS
+SELECT
+    PUBLIC.TRUNC_UDF(dt, 'Month') AS dt,
+    ROUND(AVG((temp - 273.15) * 9/5 + 32) ,0) AS AveTemp,
+    SUM(CASE
+        WHEN UPPER(RTRIM(weather_main)) in (UPPER(RTRIM('Rain')), UPPER(RTRIM('Mist')))
+            THEN 1
+            ELSE 0
+    END) AS Precip_hours
+FROM
+    DEV.SNOWCONVERT.weather
+GROUP BY 1;
+
